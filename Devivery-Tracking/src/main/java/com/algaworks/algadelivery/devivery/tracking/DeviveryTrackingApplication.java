5328173ac88b354276771705f9960637377d4b94
smunaretto <32823933+smunaretto@users.noreply.github.com>
2025-08-09 22:38:34 -0300

    Comite inicial

---
+++ Devivery-Tracking/src/main/java/com/algaworks/algadelivery/devivery/tracking/DeviveryTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algadelivery.devivery.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviveryTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviveryTrackingApplication.class, args);
+	}
+
+}
---
+++ Devivery-Tracking/src/test/java/com/algaworks/algadelivery/devivery/tracking/DeviveryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algadelivery.devivery.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviveryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
